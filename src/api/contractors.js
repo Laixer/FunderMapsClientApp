896420da9fe847109cd44dd7f97137056a5227f1
Yorick de Wid <yorick17@outlook.com>
2019-11-21 10:05:53 +0100

    Retrieve all contractors

--- src/api/contractors.js
+++ src/api/contractors.js
@@ -2,7 +2,7 @@
 import axios from '@/utils/axios'
 
 const getContractors = () => {
-  return axios.get('/api/organization/contractors')
+  return axios.get('/api/organization/contractors?limit=1000')
 }
 
 export default { 
