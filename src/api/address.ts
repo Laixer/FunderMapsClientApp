d10143f63d1491ca8afb26584bf0cd82fe62b7e8
Thomas Beckers <tabeckers95@gmail.com>
2020-09-29 14:39:40 +0200

    Created address API

---
+++ src/api/address.ts
@@ -0,0 +1,19 @@
+import axios from '../utils/axios'
+
+export default {
+    /**
+     * Lists all address suggestions for a given query.
+     * @param query The query string.
+     */
+    getAddressSuggestions(query: string) {
+        return axios.get(`/api/address/suggest?query=${query}`); // TODO Limit?
+    },
+
+    /**
+     * Gets a single address from our geocoder.
+     * @param id Internal address id.
+     */
+    getAddressById(id: string) {
+        return axios.get(`/api/address/${id}`);
+    }
+}
