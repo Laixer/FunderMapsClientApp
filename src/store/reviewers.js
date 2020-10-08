3d3d68092897414c726991ac00f8c793593b5d46
Thomas Beckers <tabeckers95@gmail.com>
2020-10-08 13:48:22 +0200

    Updated reviewer store

--- src/store/reviewers.js
+++ src/store/reviewers.js
@@ -12,31 +12,74 @@ import reviewersAPI from 'api/reviewers';
  * Declare Variable
  */
 const state = {
-  reviewers : null
+  /**
+   * Contains all reviewers. This will exclude the current
+   * user if the user has reviewing privileges.
+   */
+  validReviewers : [],
+
+  /**
+   * Contains all reviewers for this users organization.
+   */
+  reviewers: [],
 }
 
 const getters = {
-  areReviewersAvailable: state => state.reviewers !== null && state.reviewers !== undefined && state.reviewers.length > 0,
+  areReviewersAvailable: state => state.validReviewers.length > 0,
+  validReviewers: state => state.validReviewers,
   reviewers: state => state.reviewers,
   getUserById: state => ({ id }) => (state.reviewers) 
     ? state.reviewers.find(reviewer => reviewer.id === id)
     : null
 }
+
 const actions = {
-  async getReviewers({ commit }) {
+  /**
+   * Gets all reviewers for our current organization from the
+   * API. This also filters out the current user if he or she 
+   * has reviewing privileges. This result is stored to the
+   * validReviewers field.
+   */
+  async getReviewers({ commit, rootState }) {
     let response = await reviewersAPI.getReviewers();
     
     if (response.status === 200 && response.data) {
       commit('set_reviewers', { reviewers: response.data })
+
+      let filteredData = response.data.filter(r => r.id !== rootState.user.user.id);
+      commit('set_valid_reviewers', { reviewers: filteredData })
     }
   },
+
+  /**
+   * This calls getReviewers() if we can't find the requested
+   * user id in our store. If we can find it, nothing will happen.
+   */
+  async getReviewersIfNotStored({ getters, dispatch }, { userId }) {
+    if (!getters.getUserById({ id: userId })) {
+      dispatch('getReviewers');
+    }
+  },
+
   clearVersion({ commit }) {
     commit('clear_reviewers')
   }
 }
 const mutations = {
-  set_reviewers(state, { reviewers }) {
+  /**
+   * Writes the fetched reviewers to the store.
+   */
+  set_reviewers(state , { reviewers }) {
     state.reviewers = reviewers.map(
+      reviewer => new AttributedUser({ user: reviewer, role: 'Reviewer' })
+    )
+  },
+    /**
+   * Writes the fetched reviewers to the store. No filters 
+   * are applied by this function.
+   */
+  set_valid_reviewers(state , { reviewers }) {
+    state.validReviewers = reviewers.map(
       reviewer => new AttributedUser({ user: reviewer, role: 'Reviewer' })
     )
   },
