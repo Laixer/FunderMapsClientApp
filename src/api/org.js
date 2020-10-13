f6ba40cedb04bc2dd7569b238945af4c3b1e5d59
Thomas Beckers <tabeckers95@gmail.com>
2020-10-13 15:10:22 +0200

    Repaired organization admin call

--- src/api/org.js
+++ src/api/org.js
@@ -2,15 +2,30 @@
 import axios from '@/utils/axios'
 
 export default { 
+  /**
+   * Gets the organization of the currently logged in user.
+   */
   getOrganization: () => {
     return axios.get('/api/organization')
   },
+
+  /**
+   * Admin call for getting an organization by its id.
+   */
   getOrganizationById: ({ id }) => {
-    return axios.get(`/api/organization/${id}`)
+    return axios.get(`/api/admin/organization/${id}`)
   },
+
+  /**
+   * Admin call for getting all existing organizations.
+   */
   getAllOrganizations: () => {
     return axios.get('/api/admin/organization/')
   },
+
+  /**
+   * Updates the organization of teh currently logged in user.
+   */
   updateOrganization: ({ data }) => {
     // TODO This is a quickfix, see FunderMapsClientApp issue #80
     data.homeAddressNumber = data.homeAddressNumber ? Number(data.homeAddressNumber) : null;
@@ -18,6 +33,10 @@ export default {
 
     return axios.put(`/api/organization/`, data)
   },
+
+  /**
+   * Admin call updating an organization.
+   */
   updateOrganizationAsAdmin: ({ organizationId, data }) => {
     // TODO This is a quickfix, see FunderMapsClientApp issue #80
     data.homeAddressNumber = data.homeAddressNumber ? Number(data.homeAddressNumber) : null;
