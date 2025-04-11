a1eac2d50cb73b1216591fc34108cd9a64a9d20e
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-04-11 14:01:13 +0200

    Remove unused admin and map layouts from the layout component

--- src/views/layouts/layouts.vue
+++ src/views/layouts/layouts.vue
@@ -2,9 +2,7 @@
 import { Component, Vue } from "vue-property-decorator";
 import emptyLayout from "./empty.vue";
 import defaultLayout from "./default.vue";
-import adminLayout from "./admin.vue";
 import loginLayout from "./login.vue";
-import mapLayout from "./map.vue";
 
 /**
  * Determines which layout to use based on the route meta data
@@ -28,12 +26,8 @@ export default class Layouts extends Vue {
     switch (layout) {
       case "login":
         return createElement("loginLayout", this.$slots.default);
-      case "admin":
-        return createElement("adminLayout", this.$slots.default);
       case "empty":
         return createElement("emptyLayout", this.$slots.default);
-      case "map":
-        return createElement("mapLayout", this.$slots.default);
       default:
         return createElement("defaultLayout", this.$slots.default);
     }
